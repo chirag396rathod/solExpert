562ee6a8c8b49d84c8534b18494887660f02a5ff
Chirag Rathod <ahirchirag396@gmail.com>
2024-05-06 22:23:03 +0530

    Resolve Responsive design

--- src/Components/AccordionComponent/styled.jsx
+++ src/Components/AccordionComponent/styled.jsx
@@ -6,6 +6,11 @@ export const AccordionContainer = styled.div`
     font-size: 18px;
     color: #000;
     font-family: "Poppins";
+    @media screen and (max-width: 1080px) {
+      font-size: 16px;
+      font-weight: 500;
+      max-width: 94%;
+    }
   }
   .section-content {
     font-size: 16px;
@@ -34,19 +39,23 @@ export const AccordionContainer = styled.div`
       }
     }
   }
-  .accordion{
+  .accordion {
     border: none;
   }
-  .accordion__button{
+  .accordion__button {
     background: none !important;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    svg {
+      width: 16px !important;
+      height: 16px !important;
+    }
   }
-  .accordion__panel{
+  .accordion__panel {
     border-top: 1px solid #dcdcdc;
   }
-  .accordion__button:before{
+  .accordion__button:before {
     display: none;
   }
 `;
