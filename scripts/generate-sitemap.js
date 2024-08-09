f5ffac245ff3a21d937c7a18931e2ab05d9515a4
Chirag Rathod <ahirchirag396@gmail.com>
2024-08-09 22:14:24 +0530

    Add site map

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,33 @@
+// scripts/generate-sitemap.js
+import { SitemapStream, streamToPromise } from 'sitemap';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { routes } from '../src/utils/routes.js'; // Import ES module
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const baseUrl = 'https://solexpert.in';
+
+// Create a writable stream
+const sitemapStream = new SitemapStream({ hostname: baseUrl });
+
+// Create a promise that will resolve with the sitemap XML
+const sitemapPromise = streamToPromise(sitemapStream);
+
+routes.forEach(route => {
+  // Handle dynamic segments like :id and :title
+  const url = baseUrl + route.path;
+  sitemapStream.write({ url, changefreq: route.changefreq, priority: route.priority });
+});
+
+// End the stream
+sitemapStream.end();
+
+// Resolve the promise and write the sitemap to a file
+sitemapPromise.then(sitemapContent => {
+  const outputPath = path.join(__dirname, '../public', 'sitemap.xml');
+  fs.writeFileSync(outputPath, sitemapContent.toString(), 'utf8');
+  console.log('Sitemap has been generated successfully.');
+});
